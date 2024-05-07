3ee504a2fb84125971499b910707c6ee7c7e6a81
jvedrson <jvedrson@gmail.com>
2024-05-07 02:23:59 -0400

    w02 - BOM events

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,13 +1,36 @@
-const input = document.querySelector('#favchap');
-const button = document.querySelector('button');
-const list = document.querySelector('#list');
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("#list");
 
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
+function addChapter() {
+  const li = document.createElement("li");
+  const deleteButton = document.createElement("button");
 
-li.textContent = input.value;
-deleteButton.textContent = '❌';
+  li.textContent = input.value;
 
-li.appendChild(deleteButton);
+  deleteButton.textContent = "❌";
+  deleteButton.addEventListener("click", function () {
+    list.removeChild(li);
+    input.focus();
+  });
 
-list.append(li);
+  li.appendChild(deleteButton);
+
+  list.append(li);
+
+  input.value = "";
+}
+
+button.addEventListener("click", function () {
+  if (input.value.trim() !== "") {
+    addChapter();
+  }
+  input.focus();
+});
+
+input.addEventListener("keyup", function (e) {
+  if (input.value.trim() !== "" && e.keyCode == 13) {
+    addChapter();
+  }
+  input.focus();
+});
